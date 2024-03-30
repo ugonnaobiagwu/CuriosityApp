39793e60b5b2676c9bc8b9521411e5adc20e5c64
ugonnaobiagwu <50896879+ugonnaobiagwu@users.noreply.github.com>
2024-03-30 15:46:50 -0400

    Initial Commit

---
+++ Curiosity/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Curiosity
+//
+//  Created by Ugonna O on 3/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Curiosity/CuriosityApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuriosityApp.swift
+//  Curiosity
+//
+//  Created by Ugonna O on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct CuriosityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
